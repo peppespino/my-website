429d208e8efa1df6f1232418caa01f5eee4d7710
giuseppe daiello <superpeppino0000@gmail.com>
2024-10-24 16:23:53 +0200

    Inizializzazione del progetto

---
+++ web.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import function
+
+
+todos = function.get_todos()
+
+
+def add_todo():
+        todo=st.session_state['new_todo'] + '\n'
+        todos.append(todo)
+        function.write_todos(todos)
+        
+
+
+
+st.title('My Todo App')
+st.subheader('hei amici questa è la mia porcodio di app')
+st.write('questa app ti consente di ricordati tutto!!!!!')
+
+
+for index,todo in enumerate (todos):
+       checkbox= st.checkbox(todo,key=todo)
+       if checkbox:
+               todos.pop(index)
+               function.write_todos(todos)
+               del st.session_state[todo]
+               st.rerun()
+        
+        
+st.text_input(label='scrivi una frase',placeholder='scrivi qui..',
+              on_change=add_todo,key='new_todo')
+
+print('hello')
+st.session_state
