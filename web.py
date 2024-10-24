2eac92696947345600d38fb718a755d72cc74bbc
giuseppe daiello <superpeppino0000@gmail.com>
2024-10-24 20:55:11 +0200

    resa meno scurrile

--- web.py
+++ web.py
@@ -14,7 +14,7 @@ def add_todo():
 
 
 st.title('My Todo App')
-st.subheader('hei amici questa è la mia porcoddio di app')
+st.subheader('hei amici questa è la mia di app')
 st.write('''questa app ti consente di ricordati tutto!!!!!
         (anche le chiavi del CBR) ''')
 
