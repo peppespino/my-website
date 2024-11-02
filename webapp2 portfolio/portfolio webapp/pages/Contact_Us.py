773322ab7ed7f41d64ecc7b2effa265d445c460c
giuseppe daiello <superpeppino0000@gmail.com>
2024-11-02 15:41:27 +0100

    send email usando il corretto formato

--- webapp2 portfolio/portfolio webapp/pages/Contact_Us.py
+++ webapp2 portfolio/portfolio webapp/pages/Contact_Us.py
@@ -1,9 +1,20 @@
 import streamlit as st
+from send_email import send_email
+
+
 st.header('Contact Me')
 
 with st.form(key='email_forms'):
     user_email = st.text_input('Il tuo indirizzo Mail',placeholder='utente@esempio.com')
     message = st.text_area('IL tuo messaggio',placeholder='Ciao Giuseppe...')
+    #message = message + '\n' + user_email
+    message=f"""\
+Subject: New email from {user_email}
+{message}
+inviata da: {user_email}
+
+"""
     button = st.form_submit_button('Submit')
     if button:
-        print('hello')
+        send_email(message)
+        st.success("Email inviata con successo!")
--- webapp2 portfolio/portfolio webapp/send_email.py
+++ webapp2 portfolio/portfolio webapp/send_email.py
@@ -1,30 +1,23 @@
 import smtplib, ssl
 
 
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "superpeppino0000@gmail.com"
+    password = "simibxtinjspkmye"
+    receiver = "daiello.giuseppe02@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP("smtp.gmail.com") as server:
+        server.starttls() and server.login(user=username, password=password)
+        server.sendmail(from_addr=username,
+                        to_addrs=receiver,
+                        msg=message)
 
 
-host="smtp.gmail.com"
-
-port=465
 
 
-username="superpeppino0000@gmail.com"
-password= "simibxtinjspkmye"
-receiver="vegnente.chiara@gmail.com"
-
-context = ssl.create_default_context()
-message="""
-ciao!
-come va?
-ciao.
-puzzi 
 
 
-"""
 
-
-with smtplib.SMTP("smtp.gmail.com")as server:
-    server.starttls() and server.login(user=username,password=password)
-    server.sendmail(from_addr=username,
-                     to_addrs=receiver,
-                     msg=message)
